52384582acc61ab7411a72f4d5326c174a990b5d
Sandip <Sandip@DESKTOP-ARBGIQ8>
2025-09-11 01:14:50 +0530

    2nd commit for calculator demo

--- calculator-demo/src/main/java/com/example/demo/CalculatorDemoApplication.java
+++ calculator-demo/src/main/java/com/example/demo/CalculatorDemoApplication.java
@@ -3,11 +3,33 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class CalculatorDemoApplication {
 
 	public static void main(String[] args) {
+		
+        //final Logger log=LoggerFactory.getLogger(CalculatorDemoApplication.class);
+        
+        System.out.println("\n\n\n");
+       // log.info("In main()--------");
+        log.info("In main()--------");
+        log.warn("In main()--------");
+        
+        log.trace("In main()--------");
+        
+        log.warn("In main()--------");
+        log.warn("In main()--------");
+        log.error("In main()--------"); 
+        
+        log.debug("In main()--------");
+       
+        log.error("In main()--------");
 		SpringApplication.run(CalculatorDemoApplication.class, args);
+		
+		System.out.println("In main()");
 	}
 
 }
