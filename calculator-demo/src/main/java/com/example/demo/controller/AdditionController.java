c096151a6cb37d16f58cfb7edebc268d52ec57ff
SandipCodes <sandipsh321@gmail.com>
2025-09-14 23:46:15 +0530

    changes commited in AdditionController

--- calculator-demo/src/main/java/com/example/demo/controller/AdditionController.java
+++ calculator-demo/src/main/java/com/example/demo/controller/AdditionController.java
@@ -3,12 +3,18 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class AdditionController {
 
 	
 	@GetMapping
 	public String add() {
+		
+		log.info("inside of add()|| Add().....");
+		log.trace("using trace() logs");
 		return "Reached to add()";
 	}
 }
